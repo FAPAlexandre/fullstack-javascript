ad212099846b924d5be6f6196596227f06bae3fd
Alexandre Paixão <faps.job@gmail.com>
2022-12-24 21:46:08 -0300

    update

---
+++ aula08/index.js
@@ -0,0 +1,8 @@
+const nameComplete = 'Alexadre Paixão';
+const age = '39';
+const weight = '90';
+const height = '1.63'
+let imc = weight/(height * height)
+let bornYear = 2022 - age;
+console.log(`Meu nome é ${nameComplete}, nasci no ano ${bornYear} e tenho ${age} anos idade, meu peso é ${weight}Kg
+e tenho ${height}CM de altura e meu IMC é ${imc}.`)
---
+++ aula09/index.js
@@ -0,0 +1,7 @@
+//Tipos de dados primitivos
+const nome = 'Alexandre' //String
+const num1 = 1 //Number
+const num2 = 10.52 // Float
+let nomeAluno; // Undefined - Não aponta para memória
+let SobreNomeAluno = null // Nulo - Não aponta para memória
+const boolean = true // or false - logico
