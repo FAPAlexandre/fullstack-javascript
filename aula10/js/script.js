692f6d0a465e11b43402a08d2bf255ba2856f8a5
Alexandre Paixão <faps.job@gmail.com>
2022-12-25 19:54:22 -0300

    resovendo exercicio

---
+++ aula10/js/script.js
@@ -0,0 +1,7 @@
+
+const num1 = Number(prompt("Digite um número: "));
+const num2 = Number(prompt("Digite um número: "));
+
+const resultado = num1 + num2;
+
+alert(`O resutado da sua consta foi ${resultado}`)
