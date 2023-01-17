311aa5fb3115c1f91fdfbdc28f4c66d7f3d48eb7
Alexandre Paixão <faps.job@gmail.com>
2023-01-17 20:00:25 -0300

    estruturas de repetição

---
+++ logica/aula09/index.js
@@ -0,0 +1,21 @@
+const nomes = ['Alexandre', 'Paixão', 'Laura']
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i])
+    console.log('--------------------------------')
+}
+
+for ( let i in nomes) {
+    console.log(nomes[i])
+    console.log('--------------------------------')
+}
+
+for (let valor of nomes) {
+    console.log(valor)
+    console.log('--------------------------------')
+}
+
+nomes.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+
+})
