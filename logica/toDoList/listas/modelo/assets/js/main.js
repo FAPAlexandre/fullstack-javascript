182fa827be0d78412996de141fe3c5aab16fb0b2
Alexandre Paixão <faps.job@gmail.com>
2023-02-15 21:32:48 -0300

    Update toDoList

--- logica/toDoList/listas/modelo/assets/js/main.js
+++ logica/toDoList/listas/modelo/assets/js/main.js
@@ -6,6 +6,7 @@ function criaLi() {
     const li = document.createElement('li')
     return li
     
+    
 }
 
 inputTarefa.addEventListener('keypress',  function(e) {
@@ -25,6 +26,7 @@ function criaBotaoApagar(li) {
     const botaoApagar = document.createElement('button')
     botaoApagar.innerText = 'Apagar'
     li.appendChild(botaoApagar)
+    botaoApagar.setAttribute('class','apagar')
 }
 
 function criaTarefa(textoInput) {
@@ -33,9 +35,40 @@ function criaTarefa(textoInput) {
     tarefas.appendChild(li)
     limpaInput()
     criaBotaoApagar(li) 
+    salvarTarefas()
 }
 
 btnTarefa.addEventListener('click', function(){
     if(!inputTarefa.value) return
     criaTarefa(inputTarefa.value)
 })
+
+document.addEventListener('click', function(e){
+    const el = e.target
+    if(el.classList.contains('apagar')) {
+        el.parentElement.remove()
+        salvarTarefas()
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaDeTarefas = []
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
+        listaDeTarefas.push(tarefaTexto)
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefasJSON)
+ }
+ function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas')
+    const listaDeTarefas = JSON.parse(tarefas)
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa)
+    }
+ }
+ adicionaTarefasSalvas()
