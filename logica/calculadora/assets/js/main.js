f1faf1b28d4332fb01a674e5fbb730d35c0c81cb
Alexandre Paixão <faps.job@gmail.com>
2023-03-10 14:24:11 -0300

    234343

--- logica/calculadora/assets/js/main.js
+++ logica/calculadora/assets/js/main.js
@@ -1,76 +1,58 @@
-function criaCalculadora() {
-  return { // Selecionando a classe display
+function criaCalcularoda() {
+  return {
     display: document.querySelector('.display'),
 
     inicia() {
       this.cliqueBotoes();
-      this.pressionaBackSpace();
-      this.pressionaEnter();
+      this.pressionaEnter();  
     },
-
-    pressionaBackSpace() {
-      this.display.addEventListener('keydown', (e) => {
-        if (e.keyCode === 8) {
-          e.preventDefault();
-          this.clearDisplay();
-        }
-      });
+    clearDisplay() {
+      this.display.value = '';
     },
-
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+    
     pressionaEnter() {
-      this.display.addEventListener('keyup', (e) => {
-        if (e.keyCode === 13) {
-          this.realizaConta();
-        }
-      });
-    },
+        this.display.addEventListener('keyup', (e) => {
+            if (e.keyCode === 13) {
+                this.realizaConta();
+            }
+        });
+    }
 
     realizaConta() {
       let conta = this.display.value;
 
       try {
         conta = eval(conta);
-
         if (!conta) {
           alert('Conta inválida');
           return;
         }
 
-        this.display.value = (conta);
+        this.display.value = conta;
       } catch (e) {
         alert('Conta inválida');
       }
     },
 
-    clearDisplay() {
-      this.display.value = '';
-    },
-
-    apagaUm() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-
-    cliqueBotoes() { // this -> calculadora
+    cliqueBotoes() {
       document.addEventListener('click', (e) => {
-        const el = e.target; // Capiturando o botão
+        const el = e.target;
 
         if (el.classList.contains('btn-num')) {
           this.btnParaDisplay(el.innerText);
         }
-
         if (el.classList.contains('btn-clear')) {
           this.clearDisplay();
         }
-
         if (el.classList.contains('btn-del')) {
           this.apagaUm();
         }
-
         if (el.classList.contains('btn-eq')) {
-          this.realizaConta();
+          this.realizaConta(this.display.value);
         }
-
-        this.display.focus();
       });
     },
 
@@ -81,5 +63,5 @@ function criaCalculadora() {
   };
 }
 
-const calculadora = criaCalculadora();
+const calculadora = criaCalcularoda();
 calculadora.inicia();
